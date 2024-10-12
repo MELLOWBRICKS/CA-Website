c119a5813f4be2d8acc65bec21adf2f0f05185a7
Melbin M Kuriakose <100033572+MELLOWBRICKS@users.noreply.github.com>
2024-10-12 21:25:39 +0530

    Add files via upload

---
+++ UploadServlet.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.sql.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/UploadServlet")
+public class UploadServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String imageName = request.getParameter("image");
+        String comment = request.getParameter("comment");
+
+        try {
+            insertIntoDatabase(imageName, comment);
+            response.sendRedirect("memories.jsp");
+        } catch (SQLException | ClassNotFoundException e) {
+            handleException(response, e);
+        }
+    }
+
+    private void insertIntoDatabase(String imageName, String comment)
+            throws SQLException, ClassNotFoundException {
+        String jdbcURL = "jdbc:mysql://localhost/images";
+        String dbUser = "root";
+        String dbPassword = "";
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        try (Connection connection = DriverManager.getConnection(jdbcURL, dbUser, dbPassword);
+             PreparedStatement statement = connection.prepareStatement("INSERT INTO user_images (image_path, comment) VALUES (?, ?)")) {
+
+            String imagePath = "uploads/" + imageName;
+
+            statement.setString(1, imagePath);
+            statement.setString(2, comment);
+
+            statement.executeUpdate();
+        }
+    }
+    private void handleException(HttpServletResponse response, Exception e) throws IOException {
+        e.printStackTrace();
+        response.getWriter().println("Error: " + e.getMessage());
+    }
+}
